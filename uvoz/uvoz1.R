88fec9ee0416060502a4f7964198050711743fcc
vidamaver <1c.vida.maver@gmail.com>
2017-05-01 20:37:22 +0200

    Spremembe.

--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -4,7 +4,7 @@ tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']")%>%
   .[[1]] %>% html_table(dec = ".")
 
 colnames(tabela) <- c("stevnik", "drzava", "povrsina(km^2)", "opis")
-sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
-for (col in c("stevnik", "drzava", "povrsina(km^2)", "opis")) {
+sl <- locale("sl", decimal_mark = ".", grouping_mark = ",")
+for (col in c("povrsina(km^2)")) {
   tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
 }
