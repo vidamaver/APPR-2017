3b13556050cf74a1569d73748a55b7dc8a7c6361
vidamaver <1c.vida.maver@gmail.com>
2017-05-03 13:18:10 +0200

    Uvoz1

--- uvoz/uvoz1.R
+++ uvoz/uvoz1.R
@@ -3,8 +3,16 @@ stran <- html_session(link) %>% read_html()
 tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']")%>%
   .[[1]] %>% html_table(dec = ".")
 
+library(rvest)
+library(gsubfn)
+library(readr)
+library(dplyr)
+
 colnames(tabela) <- c("stevnik", "drzava", "povrsina(km^2)", "opis")
-sl <- locale("sl", decimal_mark = ".", grouping_mark = ",")
+sl <- locale("sl", decimal_mark = ".")
 for (col in c("povrsina(km^2)")) {
   tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
 }
+
+tabela$stevnik <- NULL
+tabela$opis <- NULL
