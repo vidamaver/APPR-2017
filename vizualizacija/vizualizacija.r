ed32b35f554d1292cc19c021e255149d9e0f6f0d
vidamaver <1c.vida.maver@gmail.com>
2017-05-22 12:12:06 +0200

    3. faza

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -18,14 +18,12 @@ library(readr)
 library(tibble)
 
 #--------------------------------------------------------------------
-zdruzena_tabela <- rbind(tabela2, tabela_povrsin)
 
 evropa <- uvozi.zemljevid("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
                   "ne_50m_admin_0_countries", encoding = "UTF-8") %>%
   pretvori.zemljevid() %>% filter(continent == "Europe" | sovereignt %in% c("Turkey", "Cyprus"),
-                                  long > -30)
+                                  long > -30, sovereignt != "Russia")
 
-#kako izpustim Rusijo (podatkov nimam in je ne rabim)?
 
 #prikaz zemljevida brez podatkov
 ggplot() + geom_polygon(data = evropa, aes(x = long, y = lat, group = group)) + 
@@ -35,47 +33,53 @@ ggplot() + geom_polygon(data = evropa, aes(x = long, y = lat, group = group)) +
 tabela2$drzava <- gsub("Germany.*$", "Germany", tabela2$drzava) #moji podatki
 evropa$name_sort <- gsub("^Slovak Republic$", "Slovakia", evropa$name_sort) %>% factor() #zemljevid
 
+tabela_povrsin <- rbind(tabela_povrsin, data.frame(drzava = c("Turkey", "Cyprus"),
+                                                   povrsina_v_km2 = c(783562.00, 9251.00)))
+
+
+
 #poračun skupnih izpustov na površino
-izpusti.povrsina <- tabela2 %>% group_by(drzava) %>%
-  summarise(kolicina = sum(kolicina_v_tonah)) %>% inner_join(tabela_povrsin) %>%
+izpusti.povrsina <- tabela2 %>% group_by(drzava, leto) %>%
+  summarise(kolicina = sum(kolicina_v_tonah)) %>%
+  group_by(drzava) %>% summarise(kolicina = mean(kolicina, na.rm = TRUE)) %>% inner_join(tabela_povrsin) %>%
   transmute(drzava = factor(drzava, levels = levels(evropa$name_sort)),
             izpusti = kolicina / povrsina_v_km2)
 
-#dodaj podatka o površini (a je OK?)
+#vsote čez vse panoge za vsako leto
 
-tabela_povrsin <- rbind(tabela_povrsin, "Turkey" = 783562.00)
-tabela_povrsin <- rbind(tabela_povrsin, "Cyprus" = 9251.00)
-
-#vsote čez vse panoge za vsako leto (a je OK?)
-
-tabela2 %>% group_by(leto, podrocje_industrije) %>%
-         summarise(izpusti = sum(kolicina_v_tonah, na.rm = TRUE)) %>%
-         group_by(podrocje_industrije) %>%
-         summarise(povprecni_izpusti = mean(izpusti), na.rm = TRUE)
+izpusti.podrocje <- tabela2 %>% group_by(leto, podrocje_industrije) %>%
+   summarise(izpusti = sum(kolicina_v_tonah)) %>%
+   group_by(podrocje_industrije) %>%
+   summarise(povprecni_izpusti = mean(izpusti, na.rm = TRUE))
 
 #izris zemljevida
+#Malta ima največji količnik med izpusti in površino - izločim jo iz podatkov
 ggplot() + geom_polygon(data = left_join(evropa, izpusti.povrsina,
-                                         by = c("name_sort" = "drzava")),
+                                         by = c("name_sort" = "drzava")) %>% filter(name_sort != "Malta"),
                         aes(x = long, y = lat, group = group, fill = izpusti)) +
   coord_map(xlim = c(-25, 40), ylim = c(32, 72))
 
-#Malta ima največji količnik med izpusti in površino - izločim jo iz podatkov (a je OK?)
-tabela2 %>% filter(GEO != "Malta")
 
 #-----------------------------------------------------------------------------------------------------
 
 #spreminjanje količin izpustov skozi leta za posamezno državo (po vseh tipih in vseh industrijah)
-g2 = ggplot(tabela2 %>% filter(drzava == "Slovenia") %>% group_by(leto) %>%
+g2 = ggplot(tabela2 %>% filter(drzava %in% c("Slovenia", "Croatia")) %>% group_by(leto, drzava) %>%
               summarise(izpusti = sum(kolicina_v_tonah)),
-            aes(x = leto, y = izpusti)) + geom_line()
+            aes(x = leto, y = izpusti, color = drzava)) + geom_line()
 
 g2 + xlab("leto") + ylab("kolicina_v_tonah") + ggtitle("Skupno_kolicinsko_spreminjanje_zracnih_emisij_po_drzavah")
 
+#g2 sedaj samo za SLO - rada bi za vse?
+
 #--------------------------------------------------------------------
-#spreminjanje količine posameznega tipa izpusta po vseh država in vseh panogah skupaj, po letih
+#spreminjanje količine posameznega tipa izpusta po vseh državah in vseh panogah skupaj, po letih
 
 #izpustov ogljikovega dioksida je neprimerno več od ostalih izpustov, zato ga prikažem posebaj (a je OK?)
-g31 = ggplot(tabela2 %>% filter(tip_izpusta = "Carbon dioxide")) + aes(x = leto, y = kolicina_v_tonah) + geom_line()
+g31 = ggplot(tabela2 %>% filter(tip_izpusta == "Carbon dioxide") %>%
+               group_by(leto) %>% summarise(kolicina = sum(kolicina_v_tonah, na.rm = TRUE))) +
+  aes(x = leto, y = kolicina) + geom_line()
+
+#g31 ne dela kul
 
 #preostali izpusti prikazani na enem grafu
 g32 = ggplot(tabela2 %>% filter(tip_izpusta != "Carbon dioxide") %>% group_by(leto, tip_izpusta) %>%
@@ -84,32 +88,41 @@ g32 = ggplot(tabela2 %>% filter(tip_izpusta != "Carbon dioxide") %>% group_by(le
 #pri uporabi zgornjega pazi, da so podatki še vedno primerljivi -
 # - (če bi kje manjkal podatek o kakšni državi, ki znatno vpliva na izpuste)
 
-g32 + xlab("leto") + ylab("kolicina_v_tonah") + ggtitle("Kolicinsko_spreminjanje_tipov_zracnih_emisij_po_letih (brez CO2)")
-
+#dušikovi oksidi ful nizko, dam posebaj?
 #---------------------------------------------------------------------
 #primerjava izpustov po panogah
 
-g4 = ggplot(tabela2 %>% group_by(leto, podrocje_industrije) %>%
-              summarise(izpusti = sum(kolicina_v_tonah, na.rm = TRUE)) %>%
-              group_by(podrocje_industrije) %>%
-              summarise(povprecni_izpusti = mean(izpusti), na.rm = TRUE),
-            aes(x = podrocje_industrije, y = povprecni_izpusti)) + geom_bar(stat = "identity")
+panoge <- tabela2 %>% group_by(leto, podrocje_industrije) %>%
+  summarise(izpusti = sum(kolicina_v_tonah, na.rm = TRUE)) %>%
+  group_by(podrocje_industrije) %>%
+  summarise(povprecni_izpusti = mean(izpusti)) %>% arrange(desc(povprecni_izpusti))
 
-#problem: veliko panog, dolga imena - legenda neberljiva
-#naredi več stolpčnih grafov, kjer prikažeš panoge z bolj primerljivimi izpusti - podatke pripravi vnaprej, -
-# - s filter izberi želene panoge
-#za spremembo prikazanih imen, grafu prištejem scale_x_discrete(labels = oznake) 
-# oznake = vektor želenih oznak (v slovenščini)
-# če bi oznake zarotirala prištejem še theme(axis.text.x = element_text(angle = 90, hjust = 1))
+g41 = ggplot(panoge[1:3, ],
+            aes(x = podrocje_industrije, y = povprecni_izpusti)) + geom_bar(stat = "identity") +
+  scale_x_discrete(labels = c("Oskrba z elektriko, bencinom, paro", "Proizvodnja", "Transport in skladiščenje")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+#oznake <- c("Kmetijstvo, gozdarstvo, ribolov", "Veleprodaja, prodaja na drobno, popravila motornih vozil", "Rudarstvo, kamnolom",
+#           "Gradbeništvo", "Oskrba z vodo, kanalizacija", "Javna uprava, obramba", "Dejavnosti socialnega dela in varstva za človekove pravice",
+#           "Administracija, podporne storitve", "Znanstvene in tehnične aktivnosti", "Izobraževanje", "Nastanitve, oskrba s hrano","Druge storitve", "Komunikacija",
+#           "Aktivnosti povezane z nepremičninami", "Finančne in zavarovalne aktivnosti", "Umetnost, zabava, rekreacija",
+#           "Dejavnosti gospodinjstev za lastno rabo", "Dejavnosti ekstrateritorialnih organizacij")
 
+g42 = ggplot(panoge[4:16, ],
+             aes(x = reorder(podrocje_industrije, -povprecni_izpusti), y = povprecni_izpusti)) + geom_bar(stat = "identity") +
+  scale_x_discrete(labels = c("Kmetijstvo, gozdarstvo, ribolov", "Veleprodaja, prodaja na drobno, popravila motornih vozil", 
+                              "Rudarstvo, kamnolom", "Gradbeništvo", "Oskrba z vodo, kanalizacija", "Javna uprava, obramba", 
+                              "Dejavnosti socialnega dela in varstva za človekove pravice", "Administracija, podporne storitve", 
+                              "Znanstvene in tehnične aktivnosti", "Izobraževanje", "Nastanitve, oskrba s hrano","Druge storitve", "Komunikacija")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-
-
-
-
-
-
+g43 = ggplot(panoge[17:21, ],
+             aes(x = reorder(podrocje_industrije, -povprecni_izpusti), y = povprecni_izpusti)) +
+  geom_bar(stat = "identity") +
+  scale_x_discrete(labels = c("Aktivnosti povezane z nepremičninami", "Finančne in zavarovalne aktivnosti", 
+                              "Umetnost, zabava, rekreacija", "Dejavnosti gospodinjstev za lastno rabo", 
+                              "Dejavnosti ekstrateritorialnih organizacij")) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 #---------------------------------------------------------------------
 
