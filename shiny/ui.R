b2f1fdb7ca103269c79f90cce09f7f3d47e83860
vidamaver <1c.vida.maver@gmail.com>
2017-07-11 14:06:48 +0200

    Popravki.

--- shiny/ui.R
+++ shiny/ui.R
@@ -4,7 +4,7 @@ shinyUI(fluidPage(
   titlePanel("Zračne emisije po državah"),
   sidebarPanel(
     selectInput("drzava", label = "Izberi državo:",
-                choices = c("Slovenia", parse_character(izpusti.povrsina$drzava)))
+                choices = parse_character(izpusti.povrsina$drzava), selected = 'Slovenia')
   ),
   mainPanel(plotOutput("drzava"))
 )
